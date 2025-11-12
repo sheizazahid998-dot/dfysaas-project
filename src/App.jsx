ae53d39c77e78dd2153f1da1e80ad4d1eeccee80
Sheiza <sheizazahid998@gmail.com>
2025-11-13 01:30:11 +0500

    Fix folder name capitalization for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 // All imports with lowercase folder and explicit .jsx extension
-import HeroSection from "./components/HeroSection.jsx";
-import DevelopmentSection from "./components/DevelopmentSection.jsx";
-import AdvisoryCardsSection from "./components/AdvisoryCardsSection.jsx";
-import BenefitsDFYSection from "./components/BenefitsDFYSection.jsx";
-import BlueBannerSection from "./components/BlueBannerSection.jsx";
-import OurWorkSection from "./components/OurWorkSection.jsx";
-import BestChoiceSection from "./components/BestChoiceSection.jsx";
-import TransformIdeaSection from "./components/TransformIdeaSection.jsx";
-import CustomerTestimonials from "./components/CustomerTestimonials.jsx";
-import NewsLetterSection from "./components/NewsLetterSection.jsx";
-import TwoBannerSection from "./components/TwoBannerSection.jsx";
-import FooterLinkSection from "./components/FooterLinkSection.jsx";
-import FooterContentSection from "./components/FooterContentSection.jsx";
+import HeroSection from "./Components/HeroSection.jsx";
+import DevelopmentSection from "./Components/DevelopmentSection.jsx";
+import AdvisoryCardsSection from "./Components/AdvisoryCardsSection.jsx";
+import BenefitsDFYSection from "./Components/BenefitsDFYSection.jsx";
+import BlueBannerSection from "./Components/BlueBannerSection.jsx";
+import OurWorkSection from "./Components/OurWorkSection.jsx";
+import BestChoiceSection from "./Components/BestChoiceSection.jsx";
+import TransformIdeaSection from "./Components/TransformIdeaSection.jsx";
+import CustomerTestimonials from "./Components/CustomerTestimonials.jsx";
+import NewsLetterSection from "./Components/NewsLetterSection.jsx";
+import TwoBannerSection from "./Components/TwoBannerSection.jsx";
+import FooterLinkSection from "./Components/FooterLinkSection.jsx";
+import FooterContentSection from "./Components/FooterContentSection.jsx";
 
 const App = () => {
   return (
