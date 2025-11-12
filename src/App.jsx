be9f02285a9a2fcb54a126b2ec2d3e5ff773fee6
Sheiza <sheizazahid998@gmail.com>
2025-11-13 01:15:11 +0500

    Fix HeroSection import path for Vite build

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
 import React from "react";
-import HeroSection from "./components/HeroSection";
-import DevelopmentSection from "./Components/DevelopmentSection";
-import AdvisoryCardsSection from "./Components/AdvisoryCardsSection";
-import BenefitsDFYSection from "./Components/BenefitsDFYSection";
-import BlueBannerSection from "./Components/BlueBannerSection";
-import OurWorkSection from "./Components/OurWorkSection";
-import BestChoiceSection from "./Components/BestChoiceSection";
-import TransformIdeaSection from "./Components/TransformIdeaSection";
-import CustomerTestimonials from "./Components/CustomerTestimonials";
-import NewsLetterSection from "./Components/NewsLetterSection";
-import TwoBannerSection from "./Components/TwoBannerSection";
-import FooterLinkSection from "./Components/FooterLinkSection";
-import FooterContentSection from "./Components/FooterContentSection";
 
+// All imports with lowercase folder and explicit .jsx extension
+import HeroSection from "./components/HeroSection.jsx";
+import DevelopmentSection from "./components/DevelopmentSection.jsx";
+import AdvisoryCardsSection from "./components/AdvisoryCardsSection.jsx";
+import BenefitsDFYSection from "./components/BenefitsDFYSection.jsx";
+import BlueBannerSection from "./components/BlueBannerSection.jsx";
+import OurWorkSection from "./components/OurWorkSection.jsx";
+import BestChoiceSection from "./components/BestChoiceSection.jsx";
+import TransformIdeaSection from "./components/TransformIdeaSection.jsx";
+import CustomerTestimonials from "./components/CustomerTestimonials.jsx";
+import NewsLetterSection from "./components/NewsLetterSection.jsx";
+import TwoBannerSection from "./components/TwoBannerSection.jsx";
+import FooterLinkSection from "./components/FooterLinkSection.jsx";
+import FooterContentSection from "./components/FooterContentSection.jsx";
 
 const App = () => {
   return (
     <>
       <HeroSection />
       <DevelopmentSection />
-      <AdvisoryCardsSection/>
-      <BenefitsDFYSection/>
-      <BlueBannerSection/>
-      <OurWorkSection/>
-      <BestChoiceSection/>
-      <TransformIdeaSection/>
-      <CustomerTestimonials/>
-      <NewsLetterSection/>
-      <TwoBannerSection/>
-      <FooterLinkSection/>
-      <FooterContentSection/>
+      <AdvisoryCardsSection />
+      <BenefitsDFYSection />
+      <BlueBannerSection />
+      <OurWorkSection />
+      <BestChoiceSection />
+      <TransformIdeaSection />
+      <CustomerTestimonials />
+      <NewsLetterSection />
+      <TwoBannerSection />
+      <FooterLinkSection />
+      <FooterContentSection />
     </>
   );
 };
